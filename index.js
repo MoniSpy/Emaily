07e40ae877d3af28b5444dda9fbc5484d7315f1c
Monica Spycher <monicaspycher@Monicas-MacBook-Pro.local>
2024-01-25 09:42:59 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express= require('express');
+const app= express();
+
+
+//app: object, represents the underlying running express server
+
+//route handler , will be calle anytime a request comes into the '/' route
+app.get('/',(req,res)=>{
+  res.send({hi: "there"});
+});
+
+
+//what port should we be listening to
+//process.env.PORT => look at the underlying environtment and see if they have declared a port for us to use
+const PORT = process.env.PORT || 5000;
+//express tells node to listen for traffic comin into PORT
+app.listen(PORT);
