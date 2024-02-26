4514db72a6ebf0545b25d05393af8a6f23bbf62d
Monica Spycher <monicaspycher@Monicas-MacBook-Pro.local>
2024-02-26 09:45:45 +0000

    deleted coments

--- config/keys.js
+++ config/keys.js
@@ -3,9 +3,9 @@
 
 if(process.env.NODE_ENV==='production'){
   // we are in production environtment - return the prod set of keys
-    module.exports=require('./prod');
+    module.exports=require('./prod.js');
 }
 else{
   // we are in development - return the dev keys
-  module.exports=require('./dev');
+  module.exports=require('./dev.js');
 }
--- config/prod.js
+++ config/prod.js
@@ -6,6 +6,3 @@ module.exports = {
   mongoURI:process.env.MONGO_URI,
   cookieKey:proces.env.COOKIE_KEY
 };
-// '1063382004677-pr0lbe3j72euqgijfn14d77d80vfako8.apps.googleusercontent.com'
-// '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'
-// 'mongodb+srv://monicaspycherannetts: fX1VOPNsAuJ5raet @cluster0.f45ad6o.mongodb.net/emailyprod?retryWrites=true&w=majority'
